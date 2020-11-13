25db0008171b03337725200b952488caa5452677
jack-farley <jfarley872@gmail.com>
2020-11-12 23:17:36 -0500

    adding files

---
+++ bot.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from requests_html import HTMLSession, AsyncHTMLSession
+import time
+
+import config
+
+'''
+python bot.py --name="North Face"
+'''
