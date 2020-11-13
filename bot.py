d547c93999670101bdd746073789fa5d4a7a1f71
awang9999 <alexander.wang2001@gmail.com>
2020-11-13 04:31:12 +0000

    added functions to bot

--- bot.py
+++ bot.py
@@ -3,8 +3,38 @@ from selenium.webdriver.support.ui import Select
 from requests_html import HTMLSession, AsyncHTMLSession
 import time
 
-import config
+#import config
 
 '''
-python bot.py --name="North Face"
+python bot.py --name="Play Station"
 '''
+
+
+def get_product_links():
+    pass
+
+
+def get_matched_and_available(target_name):
+    pass
+
+
+def check_can_buy(r):
+    pass
+
+
+def perform_purchase(url):
+    pass
+
+
+def main(target_product):
+    pass
+
+
+# define main
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Supremebot main parser')
+    parser.add_argument('--name', required=True,
+                        help='Specify product name to find and purchase')
+    args = parser.parse_args()
+    main(target_product=args.name)
